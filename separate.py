44d5d159ea622119c5b820b6ba13d782178e83ba
zay <juseoii@gmail.com>
2025-04-06 22:14:44 +0900

    개발중

--- separate.py
+++ separate.py
@@ -1,4 +1,5 @@
 from pyhwpx import Hwp
+import pyhwpx
 import win32clipboard
 # import tkinter as tk
 # from tkinter import filedialog
@@ -17,17 +18,43 @@ hwp = Hwp()
 hwp.Open("C:\\mjwork\\hwp_files\\250224 감정보완 2차 (판교밸리 호반써밋)_VER1.hwp")
 
 # 커서 전체 선택
-hwp.MovePos(3)  # 문서 시작점으로 이동
-hwp.HAction.Run("SelectAll")  # 전체 선택
+hwp.MovePos(1000)
+# hwp.HAction.Run("SelectAll")  # 전체 선택
 
-# 선택된 내용 복사
-hwp.HAction.Run("Copy")
+# # 선택된 내용 복사
+# hwp.HAction.Run("Copy")
+#
+# # 클립보드에서 텍스트 가져오기
+# win32clipboard.OpenClipboard()
+# data = win32clipboard.GetClipboardData()
+# win32clipboard.CloseClipboard()
+#
+# # 출력
+# print(data)
+# hwp.Quit()
 
-# 클립보드에서 텍스트 가져오기
-win32clipboard.OpenClipboard()
-data = win32clipboard.GetClipboardData()
-win32clipboard.CloseClipboard()
+i = 0
+while hwp.set_pos(0, i, 0):  # 본문의 para만 순회하면서
+    if hwp.GetHeadingString():  # 문단번호가 매겨져 있으면
+        prop = hwp.ParaShape
+        prop.SetItem("PrevSpacing", hwp.PointToHwpUnit(500))
+        hwp.ParaShape = prop
+    i += 1
 
-# 출력
-print(data)
-hwp.Quit()
+# # 개요를 찾기 위한 함수 (예시)
+# def get_outline(document):
+#     outline = []
+#     return document.ParaShape.Item("AlignType")
+#     # # 문서의 각 항목들을 검색
+#     # for section in document.sections():
+#     #     for paragraph in section.paragraphs():
+#     #         # 예시: 제목 스타일이 있는 문단을 개요로 간주
+#     #         if paragraph.style == 'Heading':
+#     #             outline.append(paragraph.text)
+#     #
+#     # return outline
+#
+#
+# # 개요 출력
+# parashape = get_outline(hwp)
+# print("문서 개요:", parashape)
